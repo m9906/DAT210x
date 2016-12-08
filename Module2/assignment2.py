5a1f9fb5e18d6cd4d5c81087d2334e99554cd8b7
m9906 <dyjeon0120@gmail.com>
2016-12-08 20:00:43 +0900

    Update assignment2.py

--- Module2/assignment2.py
+++ Module2/assignment2.py
@@ -3,18 +3,16 @@ import pandas as pd
 # TODO: Load up the 'tutorial.csv' dataset
 #
 # .. your code here ..
-
-
+csv_dataframe = pd.read_html('Module2/Datasets/tutorial.csv',sep=',')
 
 # TODO: Print the results of the .describe() method
 #
 # .. your code here ..
-
-
+table_dataframe.describe()
 
 # TODO: Figure out which indexing method you need to
 # use in order to index your dataframe with: [2:4,'col3']
 # And print the results
 #
 # .. your code here ..
-
+table_dataframe.loc[2:4,'col3']
